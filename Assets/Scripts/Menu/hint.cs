4309927a079bf3dc303cfcb947899637bcc9dd6b
Jens <vainx28@gmail.com>
2021-03-14 15:14:15 +0100

    Animation Jump Hinweis
    Flussüberquerung Hinweis hinzugefügt

--- Assets/Scripts/Menu/hint.cs
+++ Assets/Scripts/Menu/hint.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
+/*
 namespace Menu
 {
     public class hint : MonoBehaviour
@@ -22,6 +23,39 @@ namespace Menu
             {
                 //anzeige = GameObject.FindGameObjectWithTag("UIElements");
                 anzeige.SetActive(true);
+
+            }
+            Debug.Log("TRIGGER aufgerufen");
+        }
+
+    }
+}
+
+*/
+
+namespace Menu
+{
+    public class hint : MonoBehaviour
+    {
+        public GameObject Hints;
+        public GameObject anzeige1;
+        public GameObject anzeige2;
+
+
+        void Start()
+        {
+            anzeige1 = GameObject.Find("/Hints/anzeige1");
+            //anzeige2 = GameObject.Find("/Hints/anzeige2");
+            anzeige1.SetActive(false);
+            //anzeige2.SetActive(false);
+        }
+
+        void OnTriggerEnter2D(Collider2D player)
+        {
+            if (player.tag == "Player")
+            {
+                //anzeige = GameObject.FindGameObjectWithTag("UIElements");
+                anzeige1.SetActive(true);
 
             }
             Debug.Log("TRIGGER aufgerufen");
